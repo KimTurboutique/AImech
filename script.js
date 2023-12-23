e4003224052ec7292c1d9628dcf6269a166a0204
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-12-23 18:09:00 -0500

    used async/await to handle promise

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+const { OpenAI } = require('langchain/llms/openai');
+const inquirer = require('inquirer');
 require('dotenv').config();
+
+const model = new OpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    temperature: 0,
+    model: 'gpt-3.5-turbo'
+});
+
+console.log({ model });
+
+const init = async (input) => {
+
+    try {
+        const inquireResponse = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Ask a general knowledge question about your vehicle:',
+            },
+        ]); await promptFunc(inquireResponse.name);
+    }
+    catch (err) {
+        console.error(err);
+    }
+
+};
+
+init();  
