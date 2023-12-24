4859e43b6cb3fec03d8145b0b4ca3c608836392d
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-12-23 22:36:53 -0500

    had issues with parsing, declared parsers outside of functions, issue resolved

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const { OpenAI } = require('langchain/llms/openai');
 const inquirer = require('inquirer');
+const { PromptTemplate } = require("langchain/prompts");
+const { StructuredOutputParser } = require("langchain/output_parsers");
 require('dotenv').config();
 
 const model = new OpenAI({
@@ -10,8 +12,28 @@ const model = new OpenAI({
 
 console.log({ model });
 
-const init = async (input) => {
+let parser;
+let formatInstructions;
 
+const promptFunc = async (input) => {
+
+    try {
+        const res = await model.call(input);
+        console.log(res);
+
+        parser = StructuredOutputParser.fromNamesAndDescriptions({
+            code: "Simple automotive steps that answers the user's question",
+            explanation: "detailed explanation of the steps provided",
+        });
+
+        formatInstructions = parser.getFormatInstructions();
+    }
+    catch (err) {
+        console.error(err);
+    }
+};
+
+const init = async () => {
     try {
         const inquireResponse = await inquirer.prompt([
             {
@@ -19,12 +41,26 @@ const init = async (input) => {
                 name: 'name',
                 message: 'Ask a general knowledge question about your vehicle:',
             },
-        ]); await promptFunc(inquireResponse.name);
+
+            
+        ]);
+
+        await promptFunc(inquireResponse.name);
+
+        const prompt = new PromptTemplate({
+            template: "You are an automotive expert and will answer the user's questions about their vehicle thoroughly as possible.\n{question}",
+            inputVariables: ["question"],
+            partialVariables: { format_instructions: formatInstructions }
+        });
+
+        const promptInput = await prompt.format({
+            question: inquireResponse.name
+        });
     }
     catch (err) {
         console.error(err);
     }
-
 };
+
 
 init();  
